4a657873a3bde71891511d783f836b7cde6083b7
morgul27 <josemianca2000@gmail.com>
2023-09-26 10:24:34 +0200

    dasewf

---
+++ PSP/conexionesJavaPython.java
@@ -0,0 +1,24 @@
+package PSP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class conexionesJavaPython {
+    public static void main(String args[]) {
+        try {
+            String[] infoProceso = { "prueba.phyton", "proceso.phyton" }; // colocar los archivos que se quiera leer
+            Process proceso = Runtime.getRuntime().exec(infoProceso);// seleccionar el proceso que se va a ejecutar
+            int valorRetorno = proceso.waitFor();
+            if (valorRetorno == 0) {
+                System.out.println("El proceso se ha completado satisfactoriamente");
+            } else {
+                System.out.println("El proceso ha fallado. Codigo de error:" + valorRetorno);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ PSP/sincronizacion.java
@@ -0,0 +1,8 @@
+package PSP;
+
+public class sincronizacion {
+    public void main(String args[]) {
+        System.out.println("Proceso secundario...");
+        System.exit(103);
+    }
+}
